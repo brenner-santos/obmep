6ba28fb8fd720d4e4ba032a0eb9dec304b3898e3
Brenner Santos <bs.brenner.santos@gmail.com>
2024-01-24 23:20:37 +0000

    feat: cria extensao para monitorar as estatisticas das execucoes dos spiders

---
+++ obmep/extensions.py
@@ -0,0 +1,57 @@
+import json
+
+from scrapy import signals
+from sqlalchemy import Column, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.types import DATETIME, INTEGER, JSON, NUMERIC, NVARCHAR
+
+Base = declarative_base()
+
+
+class JobStats(Base):
+    __tablename__ = 'job_stats'
+    id = Column(INTEGER, primary_key=True)
+    spider = Column(NVARCHAR(20))
+    start_time = Column(DATETIME)
+    elapsed_time_seconds = Column(NUMERIC(9, 2))
+    item_scraped_count = Column(INTEGER)
+    job_stats = Column(JSON)
+
+
+class StatsPersist:
+    def __init__(self, crawler, database_url):
+        self._stats = crawler.stats
+        self._database_url = database_url
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        database_url = crawler.settings.get('OBMEP_DATABASE_URL')
+
+        o = cls(crawler, database_url)
+        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
+        crawler.signals.connect(o.spider_closed, signal=signals.spider_closed)
+        return o
+
+    def spider_opened(self, spider):
+        engine = create_engine(self._database_url)
+        Base.metadata.create_all(engine)
+        self.Session = sessionmaker(bind=engine)
+
+    def spider_closed(self, spider, reason):
+        stats = self._stats.get_stats()
+        if not 'item_scraped_count' in stats:
+            stats['item_scraped_count'] = 0
+
+        serializable_stats = json.loads(json.dumps(stats, default=str))
+
+        job_stats = JobStats(
+            spider=spider.name,
+            start_time=stats['start_time'],
+            elapsed_time_seconds=stats['elapsed_time_seconds'],
+            item_scraped_count=stats['item_scraped_count'],
+            job_stats=serializable_stats,
+        )
+        session = self.Session()
+        session.add(job_stats)
+        session.commit()
--- obmep/settings.py
+++ obmep/settings.py
@@ -1,7 +1,7 @@
 BOT_NAME = 'obmep'
 SPIDER_MODULES = ['obmep.spiders']
 NEWSPIDER_MODULE = 'obmep.spiders'
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 ITEM_PIPELINES = {
     'obmep.pipelines.DefaultValuesPipeline': 200,
     'obmep.pipelines.SQLDatabasePipeline': 500,
@@ -10,6 +10,10 @@ ITEM_PIPELINES = {
 OBMEP_DATABASE_URL = 'sqlite:///obmep.db'
 
 LOG_LEVEL = 'WARNING'
+
+EXTENSIONS = {
+    'obmep.extensions.StatsPersist': 500,
+}
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
